c81087599e5574d8644c0eb5f189327198ce86f1
roachcat <sebagiro@gmail.com>
2022-11-11 00:32:09 -0500

    port variable modified

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 const URL = "192.168.194.67";
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 router(app);
 
-const server = app.listen(PORT || 3000, () => {
+const server = app.listen(PORT, () => {
     console.log(`Servidor iniciado en el puerto http://${URL}:${PORT}`);
 })
 
